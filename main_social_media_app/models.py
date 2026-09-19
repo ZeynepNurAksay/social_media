bddd1f2c36ce60c9b1b9ea03560a62c82cce3798
ZeynepNurAksay <aksayzeynepnur@gmail.com>
2026-09-19 22:31:08 +0100

    Creating Profile model, and registering it to admin portal

--- main_social_media_app/models.py
+++ main_social_media_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+user = get_user_model()
+
+class Profile(models.Model):
+    user = models.ForeignKey(user, on_delete=models.CASCADE)
+    profile_user_id = models.IntegerField()
+    display_name = models.CharField(max_length=80, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    avatar = models.ImageField(upload_to="avatars/", default='blank_profile_image.png', blank=True)
+    website = models.URLField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.display_name or self.user.username
